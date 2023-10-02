46a7349946e0a3a69a0a9e2b1fb519806d5b9b75
TestJashim <124614385+TestJashim@users.noreply.github.com>
2023-10-02 19:39:51 +0600

    client

---
+++ SampleGrpcClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+
+namespace SampleGrpcClient
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //string serviceEndPoint = "http://localhost:5114";
+            string serviceEndPoint = "https://localhost:7023";
+            var channel = GrpcChannel.ForAddress(serviceEndPoint);
+            var client = new Greeter.GreeterClient(channel);
+            HelloRequest helloRequest = new HelloRequest
+            {
+                Name = "Jashim"
+            };
+            var response = await client.SayHelloAsync(helloRequest);
+
+            Console.WriteLine($"Response obtained from the service {response.Message}");
+        }
+    }
+}
